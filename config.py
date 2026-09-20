2f5a881a061a8c812de0acb7b672048469c47567
hieunguyen_2205 <hieu.nguyen@adurolife.com>
2026-09-20 20:32:42 +0700

    rollback config

--- config.py
+++ config.py
@@ -9,13 +9,31 @@ class Config:
 
     # Watchlist of stock tickers categorized by industry or sector
     WATCHLIST = {
-        "bat dong san": ["vic", "vhm", "agg", "cii", "ceo", "nlg", "pdr", "khg", "ntl", "tch", "dig", "dxg", "dxs"]
+        "bat dong san": ["vic", "vhm", "agg", "cii", "ceo", "nlg", "pdr", "khg", "ntl", "tch", "dig", "dxg", "dxs"],
+        "chung khoan": ["ssi", "apg", "dsc", "dse", "vck", "vix", "mbs", "vci", "bms", "cts", "fts", "vds", "shs",
+                        "vnd", "bsi", "tcx", "vpx", "hcm", "bvs"],
+        "ngan hang": ["bvb", "tpb", "msb", "nab", "tcb", "vib", "shb", "vab", "bab", "nvb", "eib", "hdb", "mbb", "ctg",
+                      "acb", "vcb", "vpb", "stb", "evf", "ocb", "bid", "lpb"],
+        "thep": ["vgs", "nkg", "hsg", "hpg"],
+        "bao hiem": ["bvh"],
+        "det may": ["msh", "tng"],
+        "ban le": ["frt", "mwg", "pnj", "pet", "dgw", "msn", "vjc", "hvn", "acv"],
+        "bds kcn": ["idc", "sip", "ntc", "szc", "szl", "vgc", "kbc", "gvr", "bcm", "tip", "phr"],
+        "cong nghe - viettel": ["fpt", "vtp", "ctr"],
+        "dien - nangluong": ["geg", "ree", "pow", "gee", "gex"],
+        "cang bien - vantai": ["sgp", "gmd", "vsc", "vos"],
+        "hoa chat - phancbon - nhua": ["dgc", "dcm", "dpm", "bfc", "las", "csv", "ntp", "bmp", "aaa"],
+        "thuc pham - thuy hai san - cao su": ["baf", "pan", "qns", "gdt", "vhc", "anv", "cmx", "dri", "trc"]
     }
 
     # Automatically flattens all tickers into a single list
     ALL_TICKERS = [t for tickers in WATCHLIST.values() for t in tickers]
 
-    US_TICKERS = ["MU", "CRCL", "NVDA"]
+    US_TICKERS = ["MU", "CRCL", "NVDA", "SNDK", "TSLA", "SPCX", "AMD", "EWY", "INTC", "MSTR", "LITE", "META", "MSFT", "PLTR", "QQQ",
+     "CBRS", "COIN", "DRAM", "GLW", "GOOGL", "NBIS", "QCOM", "SOXL", "SPY", "WDC", "SKHY", "AAOI", "ARM", "AVGO",
+     "BABA", "HOOD", "IBM", "MRVL", "NOK", "TSM", "RKLB", "AXTI", "CRWV", "INTW", "KORU", "MUU", "MVLL", "ORCL", "QNT",
+     "SNXX", "TQQQ", "AAPL", "AMAT", "AMZN", "BE", "DELL", "FLNC", "GS", "PYPL", "SMH", "SOXS", "ASML", "ASTS", "BMNR",
+     "COHR", "CRDO", "IREN", "NFLX", "SMCI", "USAR", "ALAB", "GME", "DJT"]
 
     TOKENS = [
         "BTC/USDT", "ETH/USDT", "LINK/USDT"
